f1525feb4f18131ab47851eb8d5abffa9dbb0405
Javier Rubio Gimémez <jvrr20@gmail.com>
2014-12-06 19:26:18 +0100

    Añadido script para cambiar de nivel
    Añadido script basico que usando un collider permite cambiar de nivel
    usando la "e" como tecla de accion

---
+++ Assets/Scripts/CambiarNivel.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CambiarNivel : MonoBehaviour {
+
+	public string NombreScena;
+
+	void OnTriggerStay(Collider col){
+		if (col.gameObject.name == "Player") {
+			Debug.Log ("Collider in");
+			if (Input.GetKey ("e")) {
+				Debug.Log("Input in");
+				Application.LoadLevel (NombreScena);
+			}
+		}
+	}
+}
