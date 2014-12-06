3dab6453f989804d2d15dde8d4e36d3b00b54afd
DavidRVi <davizjo@gmail.com>
2014-12-06 12:17:09 +0100

    Añadidos prefab de las notas pasado/presente

---
+++ Assets/Scripts/Character/PickUpObject.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PickUpObject : MonoBehaviour {
+
+	public GameObject playerCam;
+	
+	private Ray playerAim;
+	private GameObject objectHeld;
+	private bool isObjectHeld;
+
+	/**
+	 * 
+	 * states describing what the character is doing anytime
+	 */
+	private enum states {MOVING = 0, READING = 1};
+	private states current_state;
+
+	//private float inputLag = 1.0f;
+	
+	void Start () {
+		isObjectHeld = false;
+		objectHeld = null;
+		current_state = states.MOVING;
+	}
+	
+	void Update () {
+
+		if(current_state != states.READING)
+		{
+			if(Input.GetKeyUp(KeyCode.E)){
+				if(!isObjectHeld){
+					interactWithObject();
+				} else {
+					//holdObject();
+					dropObject();
+				}
+			}
+			if(isObjectHeld)
+			{
+				holdObject();
+			}
+		}
+		else{
+			this.GetComponent<CharacterMotor>().enabled = false;
+			if(Input.GetKeyUp(KeyCode.E)){
+				if (current_state == states.READING)
+				{
+					objectHeld.GetComponent<DisplayTextAnimated>().deactivate ();
+						
+
+				}
+				this.current_state = states.MOVING;
+				this.GetComponent<CharacterMotor>().enabled = true;
+			}
+
+		}
+	}
+
+
+		/*if(Input.GetKeyUp(KeyCode.E) && isObjectHeld){
+			isObjectHeld = false;
+			objectHeld.rigidbody.useGravity = true;
+		}*/
+	
+	private void interactWithObject(){
+		Ray playerAim = playerCam.camera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+		RaycastHit hit;
+		
+		Physics.Raycast (playerAim, out hit);
+
+		if(current_state == states.MOVING)
+		{
+			if(hit.collider.gameObject.tag.Equals("Bola"))
+			{
+				isObjectHeld = true;
+				objectHeld = hit.collider.gameObject;
+				objectHeld.rigidbody.useGravity = false;
+				//objectHeld.transform.position = this.transform.position + this.transform.forward;
+			}
+			if(hit.collider.gameObject.tag.Equals ("Nota"))
+			{
+				objectHeld = hit.collider.gameObject;
+				objectHeld.GetComponent<DisplayTextAnimated>().setActive ();
+				this.current_state = states.READING;
+			}
+		}
+
+	}
+	
+	private void holdObject(){
+		Ray playerAim = playerCam.camera.ViewportPointToRay(new Vector3(0.5f, 0.2f, 0));
+		objectHeld.transform.position = playerCam.transform.position + playerAim.direction * 0.5f;
+		objectHeld.transform.rotation = playerCam.transform.rotation;
+		//Vector3 nextPos = playerCam.transform.position + playerAim.direction * 2;
+		//Vector3 currPos = objectHeld.transform.position;
+		
+		//objectHeld.rigidbody.velocity = (nextPos - currPos) * 10;
+	}
+
+	private void dropObject(){
+		objectHeld.rigidbody.useGravity = true;
+		isObjectHeld = false;
+	}
+
+
+}
+
---
+++ Assets/Scripts/Notes/DisplayTextAnimated.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DisplayTextAnimated : MonoBehaviour {
+
+	private Text texto;
+	private float letterDelay = 0.1f;
+	private bool text_active;
+	public string contenido;
+	private float timePassed;
+
+	public Canvas level_canvas;
+
+	private int level;
+	// Use this for initialization
+	void Start () {
+		if(level_canvas == null)
+			level_canvas =  GameObject.Find ("Canvas").GetComponent<Canvas>();;
+		texto = level_canvas.GetComponentInChildren<Text>();
+		text_active = false;
+		texto.text = null;
+		//this.texto.enabled = false;
+		level = 0;
+		timePassed = letterDelay;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(text_active)
+		{
+			//if(!this.enabled)
+				//this.enabled = true;
+			if(timePassed <= 0.0f && level < contenido.Length)
+			{
+				Debug.Log(level);
+				texto.text += contenido[level];
+				level ++;
+				timePassed = letterDelay;
+			}
+			timePassed -= Time.deltaTime;
+
+		}
+	}
+
+	public void setActive(){
+		text_active = true;
+		this.texto.enabled = true;
+	}
+
+	public void deactivate(){
+		text_active = false;
+		this.texto.enabled = false;
+		texto.text = null;
+		level = 0;
+	}
+
+
+}
