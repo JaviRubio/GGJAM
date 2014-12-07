740e0d9ff48ffc656efce4b8294637de18bc8bf5
Javier Rubio Gimémez <jvrr20@gmail.com>
2014-12-07 19:01:40 +0100

    Hecha escena con puzzle 6

---
+++ Assets/Scripts/PuzzlePiano.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PuzzlePiano : MonoBehaviour {
+
+	private Vector3 startPos,endPos;
+	private float currentLerpTime;
+	public float moveDistance,lerpTime;
+
+	void Start(){
+		startPos = transform.position;
+		endPos = transform.position + transform.forward * moveDistance;
+		currentLerpTime = lerpTime;
+		}
+
+	void Update(){
+		if(Input.GetMouseButtonDown(0)){
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			Physics.Raycast(ray,out hit);
+			if(hit.collider.name==this.gameObject.name){
+				//Lerpeemos todos!!
+				//reset when we press spacebar
+				currentLerpTime = 0f;
+				}
+			}
+		//increment timer once per frame
+		currentLerpTime += Time.deltaTime;
+		if (currentLerpTime > lerpTime) {
+			currentLerpTime = lerpTime;
+		}
+		
+		//lerp!
+		float perc = currentLerpTime / lerpTime;
+		transform.position = Vector3.Lerp(startPos, endPos, Mathf.Sin (perc*4));
+		//Debug.Log ("Tecla 1 pulsada");
+		}
+	}
+
