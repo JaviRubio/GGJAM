b9b2c07f65974afa46bce0a05aa7a7f540a63cec
Eldoran <eldoran81@gmail.com>
2014-12-07 12:04:21 +0100

    Habitacion 6 Piano
    Clase para gestionar la solución del puzle 6

--- Assets/Scripts/ThreeLines/ThreeLines/H6Piano.cs
+++ Assets/Scripts/ThreeLines/ThreeLines/H6Piano.cs
@@ -1,21 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace ThreeLines
 {
     class H6Piano
     {
-        private int[] _pastSolution;
-        private int[] _presentSolution;
-        private int[] _futureSolution;
+        private readonly int[] _pastSolution;
+        private readonly int[] _presentSolution;
+        private readonly int[] _futureSolution;
+        private int _count;
 
         public H6Piano()
         {
-            _pastSolution = new []
+            _pastSolution = new[]
             {
-                
+                14, 17, 21, 19, 21, 17, 16, 14
+            };
+            _presentSolution = new[]
+            {
+                14, 17, 21, 24, 26, 23, 19, 14
+            };
+            _futureSolution = new[]
+            {
+                14, 17, 21, 19, 16, 0, 5, 7, 9
+            };
+            _count = 0;
+        }
+
+        public bool CheckPastSolved()
+        {
+            return _count == _pastSolution.Length;
+        }
+        public bool CheckPresentSolved()
+        {
+            return _count == _presentSolution.Length;
+        }
+        public bool CheckFutureSolved()
+        {
+            return _count == _futureSolution.Length;
+        }
+        public bool CheckPastNote(int note)
+        {
+            return CheckNote(_pastSolution, note);
+        }
+        public bool CheckPresentNote(int note)
+        {
+            return CheckNote(_presentSolution, note);
+        }
+        public bool CheckFutureNote(int note)
+        {
+            return CheckNote(_futureSolution, note);
+        }
+
+        private bool CheckNote(IList<int> solution, int note)
+        {
+            if (solution[_count] == note)
+            {
+                ++_count;
+                return true;
             }
+            _count = 0;
+            return false;
         }
     }
 }
