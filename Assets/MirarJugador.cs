30e97d9bb317bb9cdb1c05b7342e27c4f3523fdd
AndresPA <andres_pa@outlook.com>
2014-12-08 21:55:48 +0100

    Añadido final boss.

---
+++ Assets/MirarJugador.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MirarJugador : MonoBehaviour {
+
+	public Transform target;
+
+	// Use this for initialization
+	void Start () {
+		target = GameObject.Find("PlayerDavid").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(target != null){
+			transform.LookAt(target);
+			//this.transform.eulerAngles.z=0;
+			//this.transform.eulerAngles.x=0;
+		}
+	}
+}
